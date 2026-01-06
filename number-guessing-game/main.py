35a9464626c61bc06125a3c32a1fcc37228434f0
Brendan Coughlan <bcoughlan2404@gmail.com>
2026-01-06 13:16:53 -0500

    Added guess limit to number guessing game

--- number-guessing-game/main.py
+++ number-guessing-game/main.py
@@ -1,7 +1,8 @@
 import random
 
 if __name__ == "__main__":
-    print("Welcome to number guessing game!")
+    guesses_left = 3
+    print(f"Welcome to number guessing game! You have {guesses_left} guesses to answer the question!")
     try:
         range_minimum = int(input("Minimum: "))
         range_maximum = int(input("Maximum: "))
@@ -13,14 +14,26 @@ if __name__ == "__main__":
             try:
                 user_guess = int(input(f"Guess the number between {range_minimum} and {range_maximum}: ") )
             except:
-                print("That isn't a number!")
+                print("That isn't a number! You aren't penalized. Try again.")
             else:
                 if user_guess == random_num:
                     print("That is correct!")
                     break
                 elif user_guess < range_minimum or user_guess > range_maximum:
-                    print("That number is not in range! Try again.")
+                    print("That number is not in range! You aren't penalized. Try again.")
                 elif user_guess < random_num:
-                    print("Too low! Try again.")
+                    guesses_left -= 1
+
+                    if guesses_left == 0:
+                        print(f"You are out of guesses! The corrent answer was {random_num}. Better luck next time.")
+                        break
+
+                    print(f"Too low! You have {guesses_left} guesses left. Try again.")
                 elif user_guess > random_num:
-                    print("Too high! Try again. ")
+                    guesses_left -= 1
+
+                    if guesses_left == 0:
+                        print(f"You are out of guesses! The corrent answer was {random_num}. Better luck next time.")
+                        break
+
+                    print(f"Too high! You have {guesses_left} guesses left. Try again.")
